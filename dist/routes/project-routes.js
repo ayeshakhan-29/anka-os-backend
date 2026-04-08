c3b484e2d8ccc0222b7c6fca110321b0d40cba06
itsusamadev <99862471+ayeshakhan-29@users.noreply.github.com>
2026-04-08 23:32:11 +0200

    Add compiled project routes to dist
    Add compiled output for project routes (project-routes.js, .js.map, .d.ts, .d.ts.map) to the dist folder. The router exports project endpoints for listing, fetching by ID, creating, updating, deleting, syncing GitHub, and managing project tasks. Source maps reference the original TypeScript source (src/routes/project-routes.ts). These files are the build artifacts for the project routes.

---
+++ dist/routes/project-routes.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const project_controller_1 = require("../controllers/project-controller");
+const router = (0, express_1.Router)();
+const projectController = new project_controller_1.ProjectController();
+// Get all projects
+router.get('/', projectController.getProjects.bind(projectController));
+// Get project by ID
+router.get('/:id', projectController.getProjectById.bind(projectController));
+// Create new project
+router.post('/', projectController.createProject.bind(projectController));
+// Update project
+router.put('/:id', projectController.updateProject.bind(projectController));
+// Delete project
+router.delete('/:id', projectController.deleteProject.bind(projectController));
+// Sync GitHub repo context for a project
+router.post('/:id/sync-github', projectController.syncGithub.bind(projectController));
+// Project tasks
+router.get('/:id/tasks', projectController.getProjectTasks.bind(projectController));
+router.post('/:id/tasks', projectController.createTask.bind(projectController));
+router.put('/:id/tasks/:taskId', projectController.updateTask.bind(projectController));
+router.delete('/:id/tasks/:taskId', projectController.deleteTask.bind(projectController));
+exports.default = router;
+//# sourceMappingURL=project-routes.js.map
